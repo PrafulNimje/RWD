29e8a74fcbae97d3089f119c5b88377f3e9f4926
prafulnimje <prafulnimje1999@gmail.com>
2024-11-28 22:16:40 +0530

    new

---
+++ JavaScript/JS/function.js
@@ -0,0 +1,18 @@
+function sqr(num){
+    return num* num;
+}
+
+function cbe(num){
+    return num * num * num;
+}
+
+function evenodd(num){
+    if(num%2==0){
+        var ev = "Even"
+        return ev;
+    }
+    else{
+        var od = "Odd"
+        return od;
+    }
+}
